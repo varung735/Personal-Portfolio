4d2c201c44a7561f3b74b03f71ff6a948552c5ff
varun gupta <varung735@gmail.com>
2023-04-18 01:22:14 +0530

    getting data successfully from projects.js

---
+++ projects.js
@@ -0,0 +1,142 @@
+export const projects = {
+    "html": [
+        {
+            "id": 0,
+            "tech_used": "html",
+            "screenshot": "./assets/hashnode.svg", 
+            "title": "Street Style Landing Page",
+            "description": "Street style landing page with basic html css",
+            "github": "https://github.com/varung735/street_style_landing_page",
+            "live": "https://street-style441.netlify.app/"
+        },
+        {
+            "id": 1,
+            "tech_used": "html",
+            "screenshot": "./assets/hashnode.svg", 
+            "title": "Food Restaurant Home Page",
+            "description": "Home Page for some restaurant with basic html css",
+            "github": "https://github.com/varung735/Food_restaurant_home_page",
+            "live": "https://food-restaurant-2nd.netlify.app/"
+        },
+        {
+            "id": 2,
+            "tech_used": "html",
+            "screenshot": "./assets/hashnode.svg", 
+            "title": "Digital Marketing Home Page",
+            "description": "Home Page for some Digital Marketing Firm",
+            "github": "https://github.com/varung735/Digital-Marketing-Home-Page",
+            "live": "https://digital-marketing-4th.netlify.app/"
+        },
+        {
+            "id": 3,
+            "tech_used": "html",
+            "screenshot": "./assets/hashnode.svg", 
+            "title": "Crypto Landing Page",
+            "description": "landing page for Crypto Company with basic html css",
+            "github": "https://github.com/varung735/Crypto-Landing-Page",
+            "live": "https://crypto-landing-page-5th.netlify.app/"
+        },
+        {
+            "id": 4,
+            "tech_used": "html",
+            "screenshot": "./assets/hashnode.svg", 
+            "title": "Plant Home Page",
+            "description": "Home Page for Some Nursery with basic html css",
+            "github": "https://github.com/varung735/Plant-Home-Page",
+            "live": "https://plant-home-page-6th.netlify.app/"
+        },
+        {
+            "id": 5,
+            "tech_used": "html",
+            "screenshot": "./assets/hashnode.svg", 
+            "title": "Crypto Landing Page",
+            "description": "landing page for Crypto Company with basic html css",
+            "github": "https://github.com/varung735/Crypto-Landing-Page",
+            "live": "https://crypto-landing-page-5th.netlify.app/"
+        },
+        {
+            "id": 6,
+            "tech_used": "html",
+            "screenshot": "./assets/hashnode.svg", 
+            "title": "Product Home Page",
+            "description": "Page to display product and its features with basic html css",
+            "github": "https://github.com/varung735/Product-Home-Page",
+            "live": "https://product-home7.netlify.app/"
+        },
+        {
+            "id": 7,
+            "tech_used": "html",
+            "screenshot": "./assets/hashnode.svg", 
+            "title": "Web Design Landing Page",
+            "description": "Landing Page for some comapny which offers web design as service with basic html css",
+            "github": "https://github.com/varung735/Web-Design-Landing-Page",
+            "live": "https://web-design-proj.netlify.app/"
+        },
+        {
+            "id": 8,
+            "tech_used": "html",
+            "screenshot": "./assets/hashnode.svg", 
+            "title": "Product Home Page",
+            "description": "Page to display product and its features with basic html css",
+            "github": "https://github.com/varung735/Product-Home-Page",
+            "live": "https://product-home7.netlify.app/"
+        },
+        {
+            "id": 9,
+            "tech_used": "html",
+            "screenshot": "./assets/hashnode.svg", 
+            "title": "Developers Landing Page",
+            "description": "Home Page which offers API access for some service with basic html css",
+            "github": "https://github.com/varung735/live-project-9",
+            "live": "https://api-home-page.netlify.app/"
+        },
+        {
+            "id": 10,
+            "tech_used": "html",
+            "screenshot": "./assets/hashnode.svg", 
+            "title": "Interior Design Landing Page",
+            "description": "Landing Page for some Interior Descorating Firm with basic html css",
+            "github": "https://github.com/varung735/Interior_Decor",
+            "live": "https://interior-decor-10th.netlify.app/"
+        },
+        {
+            "id": 11,
+            "tech_used": "html",
+            "screenshot": "./assets/hashnode.svg", 
+            "title": "Hosting Landing Page",
+            "description": "Landing Page for some company which provides website hosting as service",
+            "github": "https://github.com/varung735/hosting_landing_page",
+            "live": "https://hosting-landing-page-12.netlify.app/"
+        },
+        {
+            "id": 12,
+            "tech_used": "html",
+            "screenshot": "./assets/hashnode.svg", 
+            "title": "SAAS LAnding Page",
+            "description": "Landing Page for a SAAS comapny or firm",
+            "github": "https://github.com/varung735/SASS-Landing-Page",
+            "live": "https://sass-landing-page-13th.netlify.app/"
+        },
+        {
+            "id": 13,
+            "tech_used": "html",
+            "screenshot": "./assets/hashnode.svg", 
+            "title": "Dance Home Page",
+            "description": "Landing Page for a dance academy",
+            "github": "https://github.com/varung735/Dance-Landing-Page",
+            "live": "https://dance-landing14.netlify.app/"
+        },
+        {
+            "id": 14,
+            "tech_used": "html",
+            "screenshot": "./assets/hashnode.svg", 
+            "title": "Product Design Landing Page",
+            "description": "Personal portfolio page for a product designer",
+            "github": "https://github.com/varung735/Product-design-landing-page",
+            "live": "https://product-design-landing-page-15th.netlify.app/"
+        },
+    ],
+    "JavaScript": [],
+    "react": [],
+    "backend": []
+}
--- script.js
+++ script.js
@@ -1,10 +1,55 @@
-window.addEventListener('scroll', () => {
-    let navbar = document.getElementById("nav-container");
-    let scrollValue = window.scrollY;
+import {projects} from "./projects.js";
 
-    if(scrollValue > 250){
-        navbar.style.display = 'none';
-    }else if(scrollValue >= 100){
-        navbar.style.display = 'flex';
-    }
+const projectsDiv = document.querySelector(".projects");
+
+const appendProjects = (screenshot, title, desc, github, live) => {
+    const projectCardDiv = document.createElement('div');
+    const cardHeaderDiv = document.createElement('div');
+    const cardBodyDiv = document.createElement('div');
+
+    const projectImg = document.createElement('img');
+    const projectTitle = document.createElement('h1');
+    const projectDesc = document.createElement('p');
+    const githubLink = document.createElement('button');
+    const liveLink = document.createElement('button');
+
+    projectCardDiv.classList.add('project-card');
+    cardHeaderDiv.classList.add('card-header');
+    cardBodyDiv.classList.add('card-body');
+
+    projectImg.classList.add('project-img');
+    projectImg.src = screenshot;
+
+    projectTitle.classList.add('project-title');
+    projectTitle.innerText = title;
+
+    projectDesc.classList.add('project-desc');
+    projectDesc.innerText = desc;
+
+    githubLink.classList.add('link');
+    githubLink.innerText = "GitHub";
+
+    liveLink.classList.add('link');
+    liveLink.innerText = "Live";
+
+    githubLink.addEventListener('click', () => { window.open(github, "_blank") })
+    liveLink.addEventListener('click', () => { window.open(live, "_blank") });
+
+    projectsDiv.appendChild(projectCardDiv);
+    projectCardDiv.appendChild(cardHeaderDiv);
+    cardHeaderDiv.appendChild(projectImg);
+
+    projectCardDiv.appendChild(cardBodyDiv);
+    cardBodyDiv.appendChild(projectTitle);
+    cardBodyDiv.appendChild(projectDesc);
+    
+    projectCardDiv.appendChild(githubLink);
+    projectCardDiv.appendChild(liveLink);
+}
+
+projects.html.forEach((html) => {
+    appendProjects(html.screenshot, html.title, html.description, html.github, html.live);
 });
+
+
+
